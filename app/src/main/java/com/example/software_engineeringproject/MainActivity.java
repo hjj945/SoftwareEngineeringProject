d3189728f7fbc251f7316f867c126c4d34fa8305
hjj945 <744903408@qq.com>
2020-03-26 21:24:06 +0800

    TCP单端口传送数据完成

--- app/src/main/java/com/example/software_engineeringproject/MainActivity.java
+++ app/src/main/java/com/example/software_engineeringproject/MainActivity.java
@@ -24,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
     private EditText Id_et;
     private EditText Password_et;
 
+    private  Socket socket;
+    private OutputStream os;
+    private StringBuffer sb;
+    private InputStream is;
+    private BufferedReader bufReader;
+    private InputStreamReader reader;
+    private String s;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +41,26 @@ public class MainActivity extends AppCompatActivity {
         CreateAccount_btn=(Button)findViewById(R.id.CreateAccount_btn);
         Id_et=(EditText)findViewById(R.id.Id_et);
         Password_et=(EditText)findViewById(R.id.Password_et);
+
+        //socket测试
+        new Thread() {
+            public void run(){
+                super.run();
+                try {
+                    //1.创建监听指定服务器地址以及指定服务器监听的端口号
+                    Log.e("socket", "start sending message");
+                    socket = new Socket("84.32.16.105", 12345);
+                    os = socket.getOutputStream();
+                    //2.拿到客户端的socket对象的输出流发送给服务器数据
+                    //OutputStream os = socket.getOutputStream();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
 
         //点击创建用户按钮后跳转至创建用户界面
         CreateAccount_btn.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +78,22 @@ public class MainActivity extends AppCompatActivity {
                     public void run(){
                         super.run();
                         try {
+                            Log.e("socket","sending message");
+                            os.write("Udp_Message".getBytes());
+                            os.flush();
+                            /*
+                            //拿到socket的输入流，这里存储的是服务器返回的数据
+                            is = socket.getInputStream();
+                            //解析服务器返回的数据
+                            reader = new InputStreamReader(is);
+                            bufReader = new BufferedReader(reader);
+                            s = null;
+                            sb = new StringBuffer();
+                            while((s = bufReader.readLine()) != null){
+                                sb.append(s);
+                            }
+                             */
+                            /*
                             //1.创建监听指定服务器地址以及指定服务器监听的端口号
                             Log.e("socket", "start sending message");
                             Socket socket = new Socket("84.32.16.105", 12345);
@@ -77,6 +121,7 @@ public class MainActivity extends AppCompatActivity {
                             os.close();
                             socket.close();
                             Log.e("socket", "finish sending message");
+                             */
                         } catch (UnknownHostException e) {
                             e.printStackTrace();
                         } catch (IOException e) {
@@ -87,6 +132,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("socket", "finish sending message");
+        //3、关闭IO资源（注：实际开发中需要放到finally中）
+        try {
+            /*
+            bufReader.close();
+            reader.close();
+            is.close();
+            */
+            socket.shutdownOutput();
+            os.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
