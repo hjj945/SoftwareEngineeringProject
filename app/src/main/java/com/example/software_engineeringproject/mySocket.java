9f100172ab50b0feb22e316a4052275bb82b2629
hjj945 <744903408@qq.com>
2020-04-07 14:32:53 +0800

    网络发送类封装测试

--- app/src/main/java/com/example/software_engineeringproject/mySocket.java
+++ app/src/main/java/com/example/software_engineeringproject/mySocket.java
@@ -1,4 +1,19 @@
 package com.example.software_engineeringproject;
+/************************************************************************************************
+ * 该类功能是作为网络传输接口
+ * 1.初始化：mySocket xxx = new mySocket(address,port) address设置为84.32.16.105,port 设置为12345
+ * 2.消息发送函数 send_msg. 使用方法：xxx.send_msg(message); message为需要发送的信息，类型为String
+ * 3.消息接收函数 receive_msg. 使用方法: is=xxx.receive_msg(); 该函数返回一个输入流(InputStream)对象is,
+ *   若要按行读取，需进行读取。示例如下：
+ *                     is=xxx.receive_msg()
+ *                     reader = new InputStreamReader(is);
+ *                     bufReader = new BufferedReader(reader);
+ *                     s = null;
+ *                     while((s = bufReader.readLine()) != null) {   //按行读取
+ *                         Log.e("socket", s);
+ *                     }
+ * 4.结束函数 finish_sending.使用方法：xxx.finish_sending(); 注销socket对象，停止发送与接收消息
+ */
 
 import android.util.Log;
 
@@ -58,6 +73,13 @@ public class mySocket {
     }
 
     public InputStream receive_msg(){
+        /*
+        try{
+            is = socket.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+         */
         return is;
     }
 
